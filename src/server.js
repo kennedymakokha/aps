b39ca6e48d66e3842f64a0132be3045fd6771c29
kennedymakokha <katchibo2@gmail.com>
2025-05-29 19:44:57 +0300

    initial commit

--- src/server.js
+++ src/server.js
@@ -25,89 +25,10 @@ app.get('/scan', async (req, res) => {
 });
 app.use('/api', codeRoutes);
 app.use('/api/analytics', analyticsRote);
-// app.post('/generate-qr', upload.single('logo'), async (req, res) => {
-//   try {
-//     const { ssid, password, color, background } = req.body;
-//     if (!ssid || !password) return res.status(400).json({ error: 'SSID and password required' });
-
-//     const pngBuffer = await generateQrBuffer({
-//       ssid,
-//       password,
-//       color,
-//       background,
-//       logoBuffer: req.file?.buffer
-//     });
-
-//     const base64 = 'data:image/png;base64,' + pngBuffer.toString('base64');
-//     res.json({ qrImage: base64 });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// // Endpoint to download PNG file directly
-// app.post('/download-qr', upload.single('logo'), async (req, res) => {
-//   try {
-//     const { ssid, password, color, background } = req.body;
-//     if (!ssid || !password) return res.status(400).json({ error: 'SSID and password required' });
-
-//     const pngBuffer = await generateQrBuffer({
-//       ssid,
-//       password,
-//       color,
-//       background,
-//       logoBuffer: req.file?.buffer
-//     });
-
-//     res.setHeader('Content-Disposition', 'attachment; filename=qr-code.png');
-//     res.setHeader('Content-Type', 'image/png');
-//     res.send(pngBuffer);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.post('/generate-qrtolink', upload.single('logo'), async (req, res) => {
-//   try {
-//     const { url, color, background } = req.body;
-//     if (!url) return res.status(400).json({ error: 'url required' });
-
-//     const pngBuffer = await generateLinkQrBuffer({
-//       url,
-//       color,
-//       background,
-//       logoBuffer: req.file?.buffer
-//     });
-
-//     const base64 = 'data:image/png;base64,' + pngBuffer.toString('base64');
-//     res.json({ qrImage: base64 });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.post('/download-qrtolink', upload.single('logo'), async (req, res) => {
-//   try {
-//     const { url, color, background } = req.body;
-//     if (!url) return res.status(400).json({ error: 'url required' });
-
-//     const pngBuffer = await generateLinkQrBuffer({
-//       url,
-//       color,
-//       background,
-//       logoBuffer: req.file?.buffer
-//     });
-//     res.setHeader('Content-Disposition', 'attachment; filename=qr-code.png');
-//     res.setHeader('Content-Type', 'image/png');
-//     res.send(pngBuffer);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-
+app.get("/", (req, res) => {
+  res.send("WebSocket Server is running!");
+  return
+});
 app.listen(PORT, () => {
   console.log(`Scanner API running at http://localhost:${PORT}`);
 });
